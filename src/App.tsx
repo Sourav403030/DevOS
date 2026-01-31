43ad4d3966738e407bb182f7d67c780ec1bdac65
Sourav Sanjay <souravsanjay07@gmail.com>
2026-01-31 13:13:41 +0530

    Resume component created.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import Dock from './components/Dock'
 import Nav from './components/Nav'
 import Github from './components/windows/Github'
 import Notes from './components/windows/Notes'
+import Resume from './components/windows/Resume'
 
 const App = () => {
   return (
-    <main className='h-screen w-full bg-[url(/macos-wallpaper.jpg)] bg-center bg-cover'>
+    <main className='h-screen w-full bg-[url(/macos-wallpaper.jpg)] bg-center bg-cover relative overflow-hidden'>
       <Nav/>
       <Dock/>
 
       <Github/>
       <Notes/>
+      <Resume/>
     </main>
   )
 }
---
+++ src/components/windows/Resume.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MacWindow from './MacWindow'
+
+const Resume = () => {
+  return (
+    <MacWindow>
+        <div className='h-full overflow-auto'>
+            <iframe className='h-full w-full' src="/resume.pdf" frameborder="0"></iframe>
+        </div>
+    </MacWindow>
+  )
+}
+
+export default Resume
